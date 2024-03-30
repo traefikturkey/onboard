9734f5a23aef7f42cc9072955bcb41a76ddd7e01
mike <219478+ilude@users.noreply.github.com>
2024-03-29 22:56:29 -0400

    fix error

--- app/app.py
+++ app/app.py
@@ -24,7 +24,7 @@ else:
 		'CACHE_DEFAULT_TIMEOUT': 600
 	}
 	from flask_minify import Minify
-	Minify(app=app, html=True, js=True, css=True)
+	Minify(app=app, html=True, js=True, cssless=True)
 	
 	
 cache = Cache(app, config=cache_config)
